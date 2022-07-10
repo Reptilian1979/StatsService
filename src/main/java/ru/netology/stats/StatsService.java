78fd17dfbe2f5f375fd2337fcb408a7d4c2008c7
Alex Ter <alexter1979@gmail.com>
2022-07-10 05:06:28 +0300

    Task is done

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,70 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int totalSalesCalc(int[] sales) {
+        int totalSales = 0;
+        for (int i = 0; i < sales.length; i++)
+            totalSales = totalSales + sales[i];
+        return totalSales;
+    }
+
+    public int averageSalesCalc(int[] sales) {
+        int totalSales = 0;
+        for (int i = 0; i < sales.length; i++)
+            totalSales = totalSales + sales[i];
+        int averageSales = totalSales / sales.length;
+        return averageSales;
+    }
+
+    public int maxMonthCalc(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minMonthCalc(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int badMonthsSalesCalc(int[] sales) {
+        int totalSales = 0;
+        for (int i = 0; i < sales.length; i++)
+            totalSales = totalSales + sales[i];
+        int averageSales = totalSales / sales.length;
+        int badMonths = 0;
+        for (int sale : sales) {
+            if (sale < averageSales) {
+                badMonths = badMonths + 1;
+            }
+        }
+        return badMonths;
+    }
+
+    public int goodMonthsCalc(int[] sales) {
+        int totalSales = 0;
+        for (int i = 0; i < sales.length; i++)
+            totalSales = totalSales + sales[i];
+        int averageSales = totalSales / sales.length;
+        int goodMonths = 0;
+        for (int sale : sales) {
+            if (sale > averageSales) {
+                goodMonths = goodMonths + 1;
+            }
+        }
+        return goodMonths;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,62 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void shouldCalculateTotalSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.totalSalesCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateAverageSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.averageSalesCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMaxSalesMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxMonthCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMinSalesMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minMonthCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateBadMonths() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.badMonthsSalesCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateGoodMonths() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.goodMonthsCalc(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
